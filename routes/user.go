0119ecc014e1683d8eccd770fc7215e192c0e1b9
Sirichai.Janpan <boy.sirichai@bitkub.com>
2021-08-16 00:52:51 +0700

    fixed bug

---
+++ routes/user.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func UserRoute(c *fiber.App) {
+	r := c.Group("/v1/users")
+	r.Get("", func(c *fiber.Ctx) error {
+		return c.SendString("User V1")
+	})
+
+	//e.POST("/v1/login",api.LoginUser)
+}
