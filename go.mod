b9e627ded3826615e6d2fd35b75614e8cd5fc4b4
Sirichai.Janpan <boy.sirichai@bitkub.com>
2021-08-15 21:28:12 +0700

    set port

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/suraboy/go-fiber-api
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.17.0
+require (
+	github.com/gofiber/fiber/v2 v2.17.0
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
 
-	app.Listen(":8080")
+	app.Listen(":" + port)
+
 }
