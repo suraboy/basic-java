77730406bab58ba9309f1259f6572df6fa77ffff
Sirichai.Janpan <boy.sirichai@bitkub.com>
2021-11-09 04:22:05 +0700

    rm command exc

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/suraboy/go-fiber-api/cmd"
+	"github.com/suraboy/go-fiber-api/protocol"
 )
 
 func main() {
 	fmt.Printf(`kkp-api %s, built with Go %s`, cmd.Version, cmd.GoVersion)
-	cmd.Execute()
+	protocol.ServeREST()
 }
